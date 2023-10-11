87c7a874fc92ec6e15e58bab079ac1a55485fc99
kubasamanek <jakubsamanek@gmail.com>
2023-10-11 19:29:01 +0200

    Changed troop constructors

--- src/thedrake/Troop.java
+++ src/thedrake/Troop.java
@@ -12,11 +12,15 @@ public class Troop {
     }
 
     public Troop(String name, Offset2D pivot) {
-        this(name, pivot, pivot);
+        this.name = name;
+        this.aversPivot = pivot;
+        this.reversPivot = pivot;
     }
 
     public Troop(String name) {
-        this(name, new Offset2D(1, 1), new Offset2D(1, 1));
+        this.name = name;
+        this.aversPivot = new Offset2D(1,1);
+        this.reversPivot = new Offset2D(1,1);
     }
 
     public String name() {
